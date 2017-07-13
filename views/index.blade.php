d7d1ff0962b4f3131e2e366f45d54b172bb4d8da
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2017-07-13 19:28:14 +0300

    update

--- views/index.blade.php
+++ views/index.blade.php
@@ -32,22 +32,24 @@
 
     @push(\Illuminate\Support\Facades\Config::get('tableView.dataTable.css.stack_name'))
 
-        @foreach(\Illuminate\Support\Facades\Config::get('tableView.dataTable.css.paths') as $path)
-            <link href="{{$path}}" rel="stylesheet">
-        @endforeach
+    @foreach(\Illuminate\Support\Facades\Config::get('tableView.dataTable.css.paths') as $path)
+        <link href="{{$path}}" rel="stylesheet">
+    @endforeach
 
     @endpush
 
     @push(\Illuminate\Support\Facades\Config::get('tableView.dataTable.js.stack_name'))
-        @foreach(\Illuminate\Support\Facades\Config::get('tableView.dataTable.js.paths') as $path)
-            <script src="{{$path}}"></script>
-        @endforeach
+    @foreach(\Illuminate\Support\Facades\Config::get('tableView.dataTable.js.paths') as $path)
+        <script src="{{$path}}"></script>
+    @endforeach
 
-        <script>
-            $(function () {
-                $('#{{$tableView->id()}}').DataTable();
+    <script>
+        $(function () {
+            $('#{{$tableView->id()}}').DataTable({
+                "bSort": false
             });
-        </script>
+        });
+    </script>
 
     @endpush
 
